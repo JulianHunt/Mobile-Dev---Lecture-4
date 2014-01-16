7f714e31e0b42647bc8949af4d9c0cb437bc7fb2
Julian Hunt <Julian@dhcp-39-159.cu-wireless-faculty.carleton.ca>
2014-01-16 09:35:56 -0500

    Initial Commit

---
+++ Lecture 4/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Lecture 4
+//
+//  Created by Julian Hunt on 1/16/2014.
+//  Copyright (c) 2014 Julian Hunt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
